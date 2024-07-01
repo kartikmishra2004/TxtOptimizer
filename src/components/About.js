b75fd4e86444bd8e259e5ed17c18811764ec8a87
kartikmishra2004 <kartik26nov2004@gmail.com>
2024-07-02 02:02:26 +0530

    esf

---
+++ src/components/About.js
@@ -0,0 +1,23 @@
+import React from "react";
+import Navbar from "./Navbar";
+
+export default function About() {
+  return (
+    <>
+      <h2>About Us</h2>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perferendis
+        sit, est rerum omnis eius ea reiciendis maxime quas quos repellendus
+        blanditiis quaerat, commodi, a quam magnam ullam debitis nesciunt
+        dolorem? Odio voluptatum ducimus ratione porro praesentium? Adipisci,
+        voluptates saepe quod incidunt exercitationem magnam harum tenetur
+        temporibus inventore nostrum rerum ipsa facilis ab molestiae omnis est
+        unde enim architecto dignissimos impedit? Exercitationem possimus et
+        odit architecto neque sequi minus dicta porro quae magni cumque libero
+        doloribus quo necessitatibus iure adipisci quas totam ipsam, at illo non
+        facilis aperiam alias. Distinctio fugit quo labore blanditiis beatae
+        fuga neque expedita excepturi iste quae?
+      </p>
+    </>
+  );
+}
