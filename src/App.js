e7ad6c6c5ad2deb346b7597512affe17ba278ebf
kartikmishra2004 <kartik26nov2004@gmail.com>
2024-07-02 15:07:35 +0530

    gdf

--- src/App.js
+++ src/App.js
@@ -1,12 +1,36 @@
+import { useState } from "react";
 import "./App.css";
 import Navbar from "./components/Navbar";
 import TextForm from "./components/TextForm";
+
 function App() {
+  const [Mode, setMode] = useState("light"); // This state defines that weather dark mode is enabled or disabled.
+  const [Alert, setAlert] = useState(null);
+
+  const showAlert = (message, type) => {
+    setAlert({
+      msg: message,
+      type: type 
+    });
+  }
+  const toggleMode = () => {
+    if (Mode === "light") {
+      setMode("dark");
+      document.body.style.backgroundColor = "#1a1a1a"
+      showAlert("Dark mod has been enabled","success");
+    }
+    else{
+      setMode("light");
+      document.body.style.backgroundColor = "white"
+      showAlert("Light mod has been enabled","success");
+    }
+  }
   return (
     <>
-      <Navbar title="TxtTunner"/>
+      <Navbar title="TxtOptimizer" Mode={Mode} toggleMode={toggleMode}/>
+      <Alert/>
       <div className="container my-3">
-        <TextForm heading="Enter the text below to Uppercase"/>
+        <TextForm heading="Enter the text below to optimize" Mode={Mode}/>
       </div>
     </>
   );
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -12,14 +12,27 @@ export default function TextForm(props) {
     }
     const summaryClick = () => {
         const textSummary = document.getElementById("summary");
+        const preview = document.getElementById("preview");
         textSummary.classList.remove("summaryhide");
+        preview.classList.remove("previewhide");
+    }
+    const copyClick = () => {
+      var text = document.getElementById("textBox");
+      text.select();
+      navigator.clipboard.writeText(text.value);
+    }
+    const removeExtraSpacesClick = () => {
+      let newText = text.split(/[ ]+ /);
+      setText(newText.join(" "));
     }
     const clearClick = () => {
         let newText = "";
         setText(newText);
         
         const textSummary = document.getElementById("summary");
+        const preview = document.getElementById("preview");
         textSummary.classList.add("summaryhide");
+        preview.classList.add("previewhide");
 
     }
 
@@ -33,24 +46,31 @@ export default function TextForm(props) {
   return (
     <>
     <div className="container">
-      <h2>{props.heading}</h2>
+      <h2 style={{color: props.Mode==='light'?'#1a1a1a':'white'}}>{props.heading}</h2>
       <div className="mb-3">
-        <textarea placeholder="Enter your text here" className="form-control" id="exampleFormControlTextarea1" rows="8" value={text} onChange={handleOnChange}></textarea>
+        <textarea style={{backgroundColor : props.Mode==='light'?'white':'#222222',color: props.Mode==='light'?'#1a1a1a':'white'}} className="form-control" id="textBox" rows="8" value={text} onChange={handleOnChange}></textarea>
       </div>
-      <button onClick={upperClick} className="btn btn-danger mx-2">UPPERCASE</button>
-      <button onClick={lowerClick} className="btn btn-danger mx-2">lowercase</button>
-      <button onClick={summaryClick} className="btn btn-danger mx-2" id="summary-btn">Text summary</button>
-      <button onClick={clearClick} className="btn btn-danger mx-2">Clear text</button>
+      <button onClick={upperClick} className="btn btn-danger mx-2 my-2">UPPERCASE</button>
+      <button onClick={lowerClick} className="btn btn-danger mx-2 my-2">lowercase</button>
+      <button onClick={summaryClick} className="btn btn-danger mx-2 my-2" id="summary-btn">Text summary</button>
+      <button onClick={removeExtraSpacesClick} className="btn btn-danger mx-2 my-2">Remove extra spaces</button>
+      <button onClick={copyClick} className="btn btn-danger mx-2 my-2">Copy to clipboard</button>
+      <button onClick={clearClick} className="btn btn-danger mx-2 my-2">Clear text</button>
       
 
 
       </div>
-      <div className="container my-4 summaryhide" id="summary">
+      <div className="container my-4 summaryhide" id="summary" style={{color: props.Mode==='light'?'#1a1a1a':'white'}}>
         <h2 className="my-4">Your text summary</h2>
-        <p><b>Characters : </b>{text.length} <br />
-        <b>Words : </b>{text.split(" ").length} <br />
-        <b>Sentences : </b>{text.split(".").length - 1} <br />
-        <b>Average reading time : </b> { (0.008 * text.split(" ").length).toFixed(2) } minutes</p>
+        <p><b>Characters : </b>{text.length > 0 ? text.length : "0"} <br />
+        <b>Words : </b>{text.length > 0 ? text.split(" ").length : "0"} <br />
+        <b>Sentences : </b>{text.length > 0 ? text.split(".").length -1 : "0"} <br />
+        <b>Paragraph : </b>{text.length > 0 ? text.split("\n\n").length : "0"} <br />
+        <b>Average reading time : </b> {text.length > 0 ? (0.008 * text.split(" ").length).toFixed(2) : "0" } minutes</p>
+      </div>
+      <div className="previewhide container my-4" id="preview" style={{color: props.Mode==='light'?'#1a1a1a':'white'}}>
+        <h2>Preview</h2>
+        <p>{text.length > 0 ? text : "Enter something in the textbox to preview"}</p>
       </div>
     </>
   );
